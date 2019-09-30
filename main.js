d947c732c3f9c8cac0345da11a5bd6c79a14022e
Kacper Wasiak <kacperjacekwasiak@gmail.com>
2019-09-30 14:49:49 +0200

    change in main.js: bounce the ball with paddles

--- main.js
+++ main.js
@@ -24,11 +24,11 @@ const paddlePlayer2 = {
 let tick = "";
 
 const ballMove = () => {
-  if (ball.x < paddlePlayer1.x - 1) {
-    alert('wygrał gracz 1')
-  } else if (ball.x > 9) {
-    alert('wygrał gracz numer 2')
-  }
+  // if (ball.x < paddlePlayer1.x - 1) {
+  //   alert('wygrał gracz 1')
+  // } else if (ball.x > 9) {
+  //   alert('wygrał gracz numer 2')
+  // }
   if (turnY) {
     ball.y += ballV.y
   } else if (!turnY) {
@@ -41,81 +41,86 @@ const ballMove = () => {
   };
 
 
+
 }
 
 const draw = () => {
-  for (let x = 0; x < 10; x++) {
+  if (10 > ball.x > -1) {
+    for (let x = 0; x < 10; x++) {
 
-    for (let y = 0; y < 10; y++) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
-      cell.style.left = cellWidth * x + 'px';
-      cell.style.top = cellHeight * y + 'px';
-      cell.dataset.key = x + "-" + y;
-      board.appendChild(cell);
+      for (let y = 0; y < 10; y++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style.left = cellWidth * x + 'px';
+        cell.style.top = cellHeight * y + 'px';
+        cell.dataset.key = x + "-" + y;
+        board.appendChild(cell);
+      }
     }
-  }
-  document.querySelectorAll('.cell').forEach((cell) => {
-    positionX = cell.dataset.key.slice(0, 1);
-    positionY = cell.dataset.key.slice(2, 3);
+    document.querySelectorAll('.cell').forEach((cell) => {
+      positionX = cell.dataset.key.slice(0, 1);
+      positionY = cell.dataset.key.slice(2, 3);
 
-    if (positionX === ball.x.toString() && positionY === ball.y.toString()) {
-      cell.classList.toggle('active');
-    };
+      if (positionX === ball.x.toString() && positionY === ball.y.toString()) {
+        cell.classList.toggle('active');
 
-    Array.from(paddlePlayer1.y).forEach((item) => {
-      if (item.toString() === positionY && paddlePlayer1.x.toString() === positionX) {
-        cell.classList.toggle('active')
+      };
 
-      }
+      Array.from(paddlePlayer1.y).forEach((item) => {
+        if (item.toString() === positionY && paddlePlayer1.x.toString() === positionX) {
+          cell.classList.toggle('active')
+
+        }
+      })
+      Array.from(paddlePlayer2.y).forEach((item) => {
+        if (item.toString() === positionY && paddlePlayer2.x.toString() === positionX) {
+          cell.classList.toggle('active')
+
+        }
+      })
+
     })
-    Array.from(paddlePlayer2.y).forEach((item) => {
-      if (item.toString() === positionY && paddlePlayer2.x.toString() === positionX) {
-        cell.classList.toggle('active')
-      }
-    })
+    tick = () => {
+      Array.from(paddlePlayer1.y).forEach((item) => {
+        if (item === ball.y && paddlePlayer1.x + 1 === ball.x) {
+          turnX = !turnX;
+        }
+      })
+      Array.from(paddlePlayer2.y).forEach((item) => {
+        if (item === ball.y && paddlePlayer2.x - 1 === ball.x) {
+          turnX = !turnX;
+        }
+      })
 
-  })
-  tick = () => {
-    Array.from(paddlePlayer1.y).forEach((item) => {
-      if (item === ball.y && paddlePlayer1.x === ball.x) {
-        turnX = !turnX;
-      }
-    })
-    Array.from(paddlePlayer2.y).forEach((item) => {
-      if (item === ball.y && paddlePlayer2.x === ball.x) {
-        turnX = !turnX;
-      }
-    })
 
-    // if (ball.x === 9) {
-    //   turnX = !turnX
-    // }
-    //  else if (ball.x === 1) {
-    //   turnX = !turnX
-    // }
-
-    if (ball.y === 8) {
-      turnY = !turnY;
-    } else if (ball.y === 0) {
-      turnY = !turnY;
+      if (ball.y === 8) {
+        turnY = !turnY;
+      } else if (ball.y === 0) {
+        turnY = !turnY;
+      }
     }
-    console.log(ball.x)
-    console.log(ball.y)
+
   }
 
-
+  if (ball.x < paddlePlayer1.x - 1) {
+    alert('wygrał gracz 2');
+    clearInterval(indexBallMove);
+    clearInterval(indexDraw);
+    clearInterval(indexTick);
+  } else if (ball.x > 10) {
+    alert('wygrał gracz numer 1');
+    clearInterval(indexBallMove);
+    clearInterval(indexDraw);
+    clearInterval(indexTick);
+  }
 }
-
-
-
 
 draw()
 
+const indexTick = setInterval(tick, 500);
+const indexDraw = setInterval(draw, 500);
+const indexBallMove = setInterval(ballMove, 500);
 
-setInterval(tick, 500)
-setInterval(draw, 500)
-setInterval(ballMove, 500)
 
 
 
@@ -129,7 +134,7 @@ const move = (e) => {
     let yFirstPaddlePlayer = paddlePlayer1.y.shift();
     yFirstPaddlePlayer += 3;
     paddlePlayer1.y.push(yFirstPaddlePlayer)
-    // console.log(paddlePlayer1.y)
+
 
   }
   if (e.keyCode === 87) {
@@ -137,22 +142,20 @@ const move = (e) => {
     let yLastPaddlePlayer = paddlePlayer1.y.pop();
     yLastPaddlePlayer -= 3;
     paddlePlayer1.y.unshift(yLastPaddlePlayer)
-    // console.log(paddlePlayer1.y)
+
   }
   if (e.keyCode === 38) {
     if (paddlePlayer2.y[0] === 0) return;
     let yLastPaddlePlayer = paddlePlayer2.y.pop();
     yLastPaddlePlayer -= 3;
     paddlePlayer2.y.unshift(yLastPaddlePlayer)
-    // console.log(paddlePlayer1.y)
   }
+
   if (e.keyCode === 40) {
     if (paddlePlayer2.y[2] === 9) return;
     let yFirstPaddlePlayer = paddlePlayer2.y.shift();
     yFirstPaddlePlayer += 3;
     paddlePlayer2.y.push(yFirstPaddlePlayer)
-    // console.log(paddlePlayer1.y)
-
   }
 
   draw()
